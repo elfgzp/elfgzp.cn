6d2c8b98594a0091612710634ce4fc1213cbc388
elfgzp <741424975@qq.com>
2019-05-15 14:08:02 +0800

    [MOD] Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh gzp@cloud.elfgzp.cn "cd ~/workspace/elfgzp.cn && source ~/.zshrc  && git config --global user.email '741424975@qq.com' &&
   git config --global user.name 'Gzp_'
-  git pull origin master && docker-compose build && docker-compose down && docker-compose up -d
+  git fetch -a
+  git checkout git checkout origin/gh-pages
   "
