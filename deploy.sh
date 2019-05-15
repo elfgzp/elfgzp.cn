5664f86e1c796a5ad374a50f064eb93c033091cd
elfgzp <741424975@qq.com>
2019-05-15 14:12:08 +0800

    [MOD] Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 ssh gzp@cloud.elfgzp.cn "cd ~/workspace/elfgzp.cn && source ~/.zshrc  && git config --global user.email '741424975@qq.com' &&
   git config --global user.name 'Gzp_'
   git fetch -a
-  git checkout git checkout origin/gh-pages
+  git checkout origin/gh-pages
   "
