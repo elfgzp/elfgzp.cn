7810826636698a5b7b33b04c0d024bc3d3279414
Gzp_ <741424975@qq.com>
2019-06-23 09:23:53 +0800

    [ADD] ads.txt

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git checkout --orphan gh-pages
 shopt -s extglob
+cp ads.txt _site/
 rm -rf !(_site) && mv _site/* . && rm -rf _site
 touch .nojekyll
 git config credential.helper "store --file=.git/credentials"
@@ -11,4 +12,3 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push origin gh-pages -f
-
