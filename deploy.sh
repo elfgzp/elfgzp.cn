494e700bfdf9d77a53854eacb812bb05911fbe48
Elf <me@elfgzp.cn>
2020-12-02 11:49:26 +0000

    Travis build:

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ssh gzp@www.elfgzp.cn "cd ~/workspace/elfgzp.cn && source ~/.zshrc  && git config --global user.email '741424975@qq.com' &&
+  git config --global user.name 'Gzp_'
+  git fetch -a
+  git checkout origin/gh-pages
+  "
---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout --orphan gh-pages
+shopt -s extglob
+cp ads.txt build/
+rm -rf !(build) && mv build/* . && rm -rf build
+touch .nojekyll
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push origin gh-pages -f
+git push -f --mirror https://elfgzp:${GITEE_TOKEN}@gitee.com/elfgzp/elfgzp.cn.git
